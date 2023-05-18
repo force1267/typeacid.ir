72e52b8999de88c3d3901693479b49363baec1a2
MJ Asadi <MohammadJavad.Asadi@tapsi.cab>
2023-05-19 02:36:33 +0330

    Fix dir errors

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "sync your .env file to remote:~/typeacid.ir/"
+echo "\n REMEMBER: sync your .env file to remote:~/typeacid.ir/\n"
 
 script='''
 cd typeacid.ir
@@ -8,9 +8,17 @@ cd typeacid.ir
 git stash
 git pull
 
-sh ./chatapp/apply.sh
-sh ./server/apply.sh
-sh ./nginx/apply.sh
+cd chatapp
+sh ./apply.sh
+cd ..
+cd server
+sh ./apply.sh
+cd ..
+cd nginx
+sh ./apply.sh
+cd ..
+
+echo deploy done.
 '''
 
 ssh $@ $script
