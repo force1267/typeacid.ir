e0ebb5d4e91f4cdcf6bef1f5c2806d0d4f674301
MJ Asadi <MohammadJavad.Asadi@tapsi.cab>
2023-05-19 03:50:23 +0330

    Fix dir cp problem

--- chatapp/apply.sh
+++ chatapp/apply.sh
@@ -5,6 +5,6 @@ yarn build
 
 rm -rf /var/www/html/typeacid.ir/chatapp
 mkdir -p /var/www/html/typeacid.ir/chatapp
-cp ./build/* /var/www/html/typeacid.ir/chatapp/
+cp -r ./build/* /var/www/html/typeacid.ir/chatapp/
 
 chown -hR www-data: /var/www/html/typeacid.ir
--- website/apply.sh
+++ website/apply.sh
@@ -2,6 +2,6 @@
 
 rm -rf /var/www/html/typeacid.ir/website
 mkdir -p /var/www/html/typeacid.ir/website
-cp ./* /var/www/html/typeacid.ir/website/
+cp -r ./* /var/www/html/typeacid.ir/website/
 
 chown -hR www-data: /var/www/html/typeacid.ir
